06424f3e2cd2a202e40ad2be14ac56f21d516b16
davidfarinajr <farina.d@northeastern.edu>
2021-01-26 18:18:23 -0500

    made dummy electrocatThermo library

---
+++ input/thermo/libraries/electrocatThermo.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+name = "electrocatThermo"
+shortDesc = u"Surface adsorbates on Pt(111)"
+longDesc = u"""
+Some surface species adsorbed on Pt(111),
+Mostly calculated by Katrin Blondal at Brown University around 2018,
+based on DFT calculations by Jelena Jelic at KIT.
+Note: "-h" means "horizontal".
+"""
+
+
+entry(
+    index = 1,
+    label = "vacant",
+    molecule =
+"""
+1 X  u0 p0 c0
+""",
+    thermo = NASA(
+        polynomials = [
+            NASAPolynomial(coeffs=[
+             0.000000000E+00,   0.000000000E+00,   0.000000000E+00,   0.000000000E+00,
+             0.000000000E+00,   0.000000000E+00,   0.000000000E+00], Tmin=(298.0,'K'), Tmax=(1000.0, 'K')),
+            NASAPolynomial(coeffs=[
+             0.000000000E+00,   0.000000000E+00,   0.000000000E+00,   0.000000000E+00,
+             0.000000000E+00,   0.000000000E+00,   0.000000000E+00], Tmin=(1000.0,'K'), Tmax=(3000.0, 'K')),
+        ],
+        Tmin = (298.0, 'K'),
+        Tmax = (3000.0, 'K'),
+    ),
+    metal = "Pt",
+    facet = "111",
+)
+
+entry(
+    index = 2,
+    label = "electron",
+    molecule =
+"""
+1 e u0 p0 c-1
+""",
+    thermo = NASA(
+        polynomials = [
+            NASAPolynomial(coeffs=[
+             0.000000000E+00,   0.000000000E+00,   0.000000000E+00,   0.000000000E+00,
+             0.000000000E+00,   0.000000000E+00,   0.000000000E+00], Tmin=(298.0,'K'), Tmax=(1000.0, 'K')),
+            NASAPolynomial(coeffs=[
+             0.000000000E+00,   0.000000000E+00,   0.000000000E+00,   0.000000000E+00,
+             0.000000000E+00,   0.000000000E+00,   0.000000000E+00], Tmin=(1000.0,'K'), Tmax=(3000.0, 'K')),
+        ],
+        Tmin = (298.0, 'K'),
+        Tmax = (3000.0, 'K'),
+    ),
+    metal = "Pt",
+    facet = "111",
+)
+
+entry(
+    index = 3,
+    label = "proton",
+    molecule =
+"""
+1 H u0 p0 c+1
+""",
+    thermo = NASA(
+        polynomials = [
+            NASAPolynomial(coeffs=[
+             0.000000000E+00,   0.000000000E+00,   0.000000000E+00,   0.000000000E+00,
+             0.000000000E+00,   0.000000000E+00,   0.000000000E+00], Tmin=(298.0,'K'), Tmax=(1000.0, 'K')),
+            NASAPolynomial(coeffs=[
+             0.000000000E+00,   0.000000000E+00,   0.000000000E+00,   0.000000000E+00,
+             0.000000000E+00,   0.000000000E+00,   0.000000000E+00], Tmin=(1000.0,'K'), Tmax=(3000.0, 'K')),
+        ],
+        Tmin = (298.0, 'K'),
+        Tmax = (3000.0, 'K'),
+    ),
+    metal = "Pt",
+    facet = "111",
+)
